9d8479189d9a001b862358f57a31720101f4b261
Kumar Chinnadurai <Kishore.KumarChinnadurai@scientificgames.com>
2018-04-20 10:05:32 +0530

    Inital Push

---
+++ js/custom.js
@@ -0,0 +1,39 @@
+$(function () {
+    //smooth scroll
+    $('body').scrollspy({
+        target: '.navbar',
+        offset: $(window).height() / 2
+    });
+
+    //auto close nav bar after click on link on mobile view
+    $('.navbar-nav>li>a').on('click', function () {
+        $('.navbar-collapse').collapse('hide');
+    });
+
+    //smooth scroll of the body navigation
+    // $("#navbarCollapse a").on('click', function (event) {
+    //     if (this.hash !== "") {
+    //         event.preventDefault();
+    //         var hash = this.hash;
+    //         $('html, body').animate({
+    //             scrollTop: $(hash).offset().top
+    //         }, 800, function () {
+    //             window.location.hash = hash;
+    //         });
+    //     }
+    // });
+
+    //Navbar Toggle button animation
+    $('#toggler').click(function () {
+        $('.animated-icon').toggleClass('open');
+    });
+    $('#navbarCollapse').click(function () {
+        $('.animated-icon').toggleClass('open');
+    });
+
+    //scroll event
+    //ToDo write the scroll logic here
+    $(window).on('scroll',function(){
+        var vScroll = $(this).scrollTop();
+    });
+});
