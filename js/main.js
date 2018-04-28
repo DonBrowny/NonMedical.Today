099eb78ec6789e07f21abf7c21b5d8d658fd0bff
guhan murugesan <mguhan.rmd@gmail.com>
2018-04-28 08:44:30 +0530

    product info modal UI change

--- js/main.js
+++ js/main.js
@@ -1,20 +1,21 @@
 var speed = 50; /* The speed/duration of the effect in milliseconds */
-
+var typeText="";
+var typeObj;
 $(document).ready(function()
 {
     $('#portfolioSlide').bind('slide.bs.carousel', function (e) {
-        typeWriter($(this).find('.active'));
+        i = 0;
+        typeText = $(this).find('h4').html();
+        typeObj = $(this).find('h4');
+        $(this).find('h4').html('');
+        typeWriter();
     });
 });
 
-function typeWriter(obj) {
-    var txt = "";
-    txt = $(obj).find('h4').html();
-    $(obj).find('h4').html("")
-    var i=0;
-  if (i < txt.length) {
-    $(obj).html($(obj).html() + txt.charAt(i));
-    i++;
-    setTimeout(typeWriter, speed);
+function typeWriter() {
+    if (i < typeText.length) {
+         $(typeObj).html($(typeObj).html() + typeText.charAt(i));
+         i++;
+        setTimeout(typeWriter, speed);
   }
 }
