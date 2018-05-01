cae6a2297847e2c35fad37fa6c6b07e6bb986be5
HellCat <kishore13ask@gmail.com>
2018-05-01 21:08:59 +0530

    added events

--- js/custom.js
+++ js/custom.js
@@ -1,17 +1,38 @@
 $(function () {
-    $(".owl-carousel").owlCarousel({
-        loop:true,
-        items:1,
-        nav:true,
-        loop:true,
-        autoplay:true,
+    //carousel
+    var owl = $(".owl-carousel").owlCarousel({
+        loop: true,
+        items: 1,
+        nav: true,
+        loop: true,
+        autoplay: true,
         animateOut: 'slideOutDown',
         animateIn: 'flipInX',
-        smartSpeed:450,
-        autoplayTimeout:5000,
-        autoplayHoverPause:true,
-        navText:['<img width="50" height="50"  src="./images/carousel-arrow-left.png">','<img width="50" height="50" src="./images/carousel-arrow-right.png">']
+        navSpeed:500,
+        smartSpeed: 450,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true
+        //onTranslate : carouselChanges,
+        // navText:['<img width="50" height="50"  src="./images/carousel-arrow-left.png">','<img width="50" height="50" src="./images/carousel-arrow-right.png">']
     });
+
+    function carouselChanges(event) {
+        // $('#captionDiv').removeClass('bounce');
+        // $('#captionDiv').addClass('');
+        console.log(event);
+        var currentItem = $('.owl-item', owl).eq(event.item.index);
+        $('#captionDiv').addClass('py-10 text-info animated lightSpeedIn HC');
+    }
+
+    owl.on('change.owl.carousel', function (event) {
+        const animationEndEvent = 'webkitAnimationEnd mozAnimationEnd MSAnimationEnd oanimationend animationend';
+        // var currentItem = $('.owl-item', owl).eq(event.item.index);
+        var currentItem = $('#captionDiv');
+        currentItem.addClass('bounce animated').one(animationEndEvent, function () {
+            currentItem.removeClass('bounce'); // remove animate.css Class at the end of the animations
+        });
+    });
+
     //smooth scroll
     $('body').scrollspy({
         target: '.navbar',
@@ -24,34 +45,33 @@ $(function () {
     });
 
     //smooth scroll of the body navigation
-    // $("#navbarCollapse a").on('click', function (event) {
-    //     if (this.hash !== "") {
-    //         event.preventDefault();
-    //         var hash = this.hash;
-    //         $('html, body').animate({
-    //             scrollTop: $(hash).offset().top
-    //         }, 800, function () {
-    //             window.location.hash = hash;
-    //         });
-    //     }
-    // });
+    $("#navbarCollapse a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 800, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
 
     //Navbar Toggle button animation
     $('.navbar-toggler').on('click', function () {
-		$('.animated-icon').toggleClass('open');
-	});
-	$('.nav-link').on('click', function () {
-		$('.animated-icon').toggleClass('open');
-	});	
+        $('.animated-icon').toggleClass('open');
+    });
+    $('.nav-link').on('click', function () {
+        $('.animated-icon').toggleClass('open');
+    });
 
     //scroll event
     $(window).on('scroll', function () {
         let vScroll = $(this).scrollTop();
-        if(vScroll > 100){
+        if (vScroll > 600) {
             $('#nav').removeClass('navbar-custom');
             $('#nav').addClass('bg-dark');
-        }  
-        else{
+        } else {
             $('#nav').addClass('navbar-custom');
             $('#nav').removeClass('bg-dark');
         }
