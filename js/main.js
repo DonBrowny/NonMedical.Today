d2d8cd311fa343c84dc12d60d9f1a503d98b5f73
HellCat <kishore13ask@gmail.com>
2018-05-19 15:13:00 +0530

    Bug fixes
    1 Logo Image Asaptive
    2 Customer Contact position change
    3 Merged About Us page into the main.html

--- js/main.js
+++ js/main.js
@@ -1,19 +1,17 @@
-var speed = 50; /* The speed/duration of the effect in milliseconds */
-var typeText="";
+var speed = 25; /* The speed/duration of the effect in milliseconds */
+var typeText = "";
 var typeObj;
-$(document).ready(function()
-{
+$(document).ready(function () {
     $('#portfolioSlide').bind('slid.bs.carousel	', function (e) {
-        setTimeout(SetTypeWriter($(this)),100);
+        setTimeout(SetTypeWriter($(this)), 100);
     });
     SetTypeWriter($('#portfolioSlide'));
-    $('.modalContactUs').on('click',goToContact);
+    $('.modalContactUs').on('click', goToContact);
     $('.collapse').on('hidden.bs.collapse', closeCollapse);
-     $('.collapse').on('shown.bs.collapse', openCollapse);
+    $('.collapse').on('shown.bs.collapse', openCollapse);
 });
 
-function SetTypeWriter(obj)
-{
+function SetTypeWriter(obj) {
     i = 0;
     typeText = $(obj).find('.active').find('.hidMessage').val();
     typeObj = $(obj).find('.active').find('p');
@@ -21,28 +19,26 @@ function SetTypeWriter(obj)
     typeWriter();
 }
 
-function goToContact()
-{
-    setTimeout(function(){
-        SmoothScrollTo('contact',1000);
-    },500); 
+function goToContact() {
+    setTimeout(function () {
+        SmoothScrollTo('contact', 1000);
+    }, 500);
 }
 
-function openCollapse(e)
-{
-    $(e.target).prev('.headerCollapse').find('i').attr('class','floatRight fas fa-minus');
+function openCollapse(e) {
+    $(e.target).prev('.headerCollapse').find('i').attr('class', 'floatRight fas fa-minus');
 }
 
-function closeCollapse(e)
-{
-    $(e.target).prev('.headerCollapse').find('i').attr('class',' floatRight fas fa-plus');
+function closeCollapse(e) {
+    $(e.target).prev('.headerCollapse').find('i').attr('class', ' floatRight fas fa-plus');
 }
+
 function typeWriter() {
     if (i < typeText.length) {
-         $(typeObj).text($(typeObj).text() + typeText.charAt(i));
-         i++;
+        $(typeObj).text($(typeObj).text() + typeText.charAt(i));
+        i++;
         setTimeout(typeWriter, speed);
-  }
+    }
 }
 
 function toggleIcon(e) {
@@ -52,12 +48,12 @@ function toggleIcon(e) {
         .toggleClass('glyphicon-plus glyphicon-minus');
 }
 
-function SmoothScrollTo(id_or_Name, timelength){
- console.log('id:'+id_or_Name);
+function SmoothScrollTo(id_or_Name, timelength) {
+    console.log('id:' + id_or_Name);
     var timelength = timelength || 1000;
     $('html, body').animate({
-        scrollTop: $("#"+id_or_Name).offset().top-70
-    }, timelength, function(){
+        scrollTop: $("#" + id_or_Name).offset().top - 70
+    }, timelength, function () {
         window.location.hash = id_or_Name;
     });
 }
