257bd8c1531e6a58bc026de56451e06be1bc64af
guhan murugesan <mguhan.rmd@gmail.com>
2018-05-21 20:49:41 +0530

    minor chnages

--- index.php
+++ index.php
@@ -1,4 +1,18 @@
 <?php
+
+function Redirect($url, $permanent = false)
+{
+	header('Location: ' . $url, true, $permanent ? 301 : 302);
+
+	exit();
+}
+
+function LogUserData($data)
+{
+	$myfile = fopen("nonmedical.today.log.txt", "w") or die("Unable to open file!");
+	fwrite($myfile, $txt);
+	fclose($myfile);
+}
 	if (isset($_POST["submit"])) {
 		$name = $_POST['name'];
 		$email = $_POST['email'];
@@ -8,9 +22,13 @@
 		$from = 'form@nonmedical.byethost24.com'; 
 		$to = 'mguhan.rmd@gmail.com'; 
 		$subject = 'Message from Contact Demo ';
-		
+		$errName = "";
+		$errEmail = "";
+		$errMessage = "";
+		$errHuman = "";
+
 		$body = "From: $name\n E-Mail: $email\n Age: $age\n Gender: $optradio";
- 
+		LogUserData($body);
 		// Check if name has been entered
 		if (!$_POST['name']) {
 			$errName = 'Please enter your name';
@@ -29,13 +47,21 @@
 			$errMessage = 'Please enter your message';
 		}
  
-// If there are no errors, send the email
-if (!$errName && !$errEmail && !$errMessage && !$errHuman) {
-	if (mail ($to, $subject, $body, $from)) {
-		$result='<div class="alert alert-success">Thank You! I will be in touch</div>';
-	} else {
-		$result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later</div>';
+	// If there are no errors, send the email
+	if (!$errNames && !$errEmail && !$errMessage && !$errHuman) {
+		try{
+			if (mail ($to, $subject, $body, $from)) {
+				$result='<div class="alert alert-success">Thank You! I will be in touch</div>';
+			} else {
+				$result='<div class="alert alert-danger">Sorry there was an error sending your message. Please try again later</div>';
+			}
+			LogUserData("Mail Success")
+		}
+		catch(Exception $e)
+		{
+			LogUserData("Mail Exception:"+$e->getMessage())
+		}
 	}
-}
 	}
+	Redirect('home.html', false);
 ?>
